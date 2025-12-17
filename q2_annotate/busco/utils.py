0603dddf538781ac794cb252e7a7d1f3aed4b4a5
Felicia Sandberg <felicia.sandberg@hest.ethz.ch>
2025-12-17 16:18:06 +0100

    Remove unused import warnings

--- q2_annotate/busco/utils.py
+++ q2_annotate/busco/utils.py
@@ -7,7 +7,6 @@
 # ----------------------------------------------------------------------------
 import json
 import os
-import warnings
 from pathlib import Path
 from typing import List, Callable
 
